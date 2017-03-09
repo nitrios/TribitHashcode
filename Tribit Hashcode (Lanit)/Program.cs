7fa2dc3e2f348be527ce50192f8cdfde996271d2
mega mix <megamixman@outlook.com>
2017-03-09 17:14:10 +0700

    init

---
+++ Tribit Hashcode (Lanit)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tribit_Hashcode__Lanit_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
