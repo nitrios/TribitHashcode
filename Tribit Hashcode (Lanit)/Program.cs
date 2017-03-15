0855c7767f543d843577f2f9b0fff4b2d4a2f7d5
mega mix <megamixman@outlook.com>
2017-03-15 15:13:52 +0700

    reduce in one step

--- Tribit Hashcode (Lanit)/Program.cs
+++ Tribit Hashcode (Lanit)/Program.cs
@@ -6,7 +6,7 @@ namespace Tribit
     class Program
     {
         private const int CellSize = 4;
-        private static readonly Dictionary<string, string> Rules = new Dictionary<string, string>
+        private static readonly IDictionary<string, string> Rules = new Dictionary<string, string>
         {
             {"0000", "0000"},
             {"0001", "1000"},
@@ -37,14 +37,14 @@ namespace Tribit
                 t = t * CellSize;
             }
 
-            if ((int)Math.Pow(CellSize, pow) != lenght)
-                throw new ArgumentException(string.Format("The number of cells '{0}' is not a power at {1}!", input, CellSize));
+            if (lenght != 1 && (int) Math.Pow(CellSize, pow) != lenght)
+                throw new ArgumentException(string.Format("The number of cells '{0}' is not a power at {1}!", input,
+                    CellSize));
 
-            var rowCount = (int)Math.Pow(2, pow);
+            var rowCount = (int) Math.Pow(2, pow);
             var pyramid = new char[rowCount][];
 
             var position = 0;
-
             for (var i = rowCount - 1; i >= 0; i--)
             {
                 var symbolsCount = (i + 1) * 2 - 1;
@@ -57,71 +57,92 @@ namespace Tribit
                 }
             }
 
-            Console.WriteLine(PyramidToString(pyramid));
-            if (lenght == 1)
-                return pyramid;
+            return pyramid;
+        }
 
-            var cellPosition = 0;
-            var cellCount = lenght / CellSize;
-            var cells = new string[cellCount];
-            for (var i = 0; i < cellCount; i++)
-                cells[i] = string.Empty;
+        private static char[][] Convert(char[][] pyramid, int length)
+        {
+            var reduced = false;
+            var canReduce = true;
 
-            var wasConverted = false;
-            for (var i = 0; i < pyramid.Length / 2; i++)
+            string[] cells = {};
+            int[,] coords = {};
+
+            while (canReduce)
             {
-                var currentRowCellCount = i * 2 + 1;
-                var isUpDown = false;
+                var cellPosition = 0;
+                var cellCount = length / CellSize;
 
-                for (var j = 0; j < currentRowCellCount; j++)
+                coords = new int[cellCount, 4];
+                cells = new string[cellCount];
+                for (var i = 0; i < cellCount; i++)
+                    cells[i] = string.Empty;
+
+                for (var i = 0; i < pyramid.Length / 2; i++)
                 {
-                    var pRow = i * 2;
-                    var pSecondRow = pRow + 1;
-
-                    var pPosition = j * 2;
-                    var pSecondPosition = pPosition;
-
-                    if (isUpDown)
+                    var currentRowCellCount = i * 2 + 1;
+                    for (var j = 0; j < currentRowCellCount; j++)
                     {
-                        pRow++;
-                        pSecondRow--;
+                        var pRow = i * 2;
+                        var pSecondRow = pRow + 1;
 
-                        pPosition++;
-                        pSecondPosition--;
+                        var pPosition = j * 2;
+                        var pSecondPosition = pPosition;
+
+                        if (j % 2 == 1)
+                        {
+                            pRow++;
+                            pSecondRow--;
+
+                            pPosition++;
+                            pSecondPosition--;
+                        }
+
+                        coords[cellPosition, 0] = pRow;
+                        coords[cellPosition, 1] = pPosition;
+                        coords[cellPosition, 2] = pSecondRow;
+                        coords[cellPosition, 3] = pSecondPosition;
+
+                        cells[cellPosition] = string.Format("{0}{1}{2}{3}",
+                            pyramid[pSecondRow][pSecondPosition + 2],
+                            pyramid[pSecondRow][pSecondPosition + 1],
+                            pyramid[pSecondRow][pSecondPosition + 0],
+                            pyramid[pRow][pPosition]);
+
+                        if (cells[cellPosition] != "1111" && cells[cellPosition] != "0000")
+                            canReduce = false;
+
+                        cellPosition++;
                     }
+                }
 
-                    var cellString = string.Format("{0}{1}{2}{3}",
-                        pyramid[pSecondRow][pSecondPosition + 2],
-                        pyramid[pSecondRow][pSecondPosition + 1],
-                        pyramid[pSecondRow][pSecondPosition + 0],
-                        pyramid[pRow][pPosition]);
+                if (canReduce)
+                {
+                    reduced = true;
 
-                    if (cellString != "1111" && cellString != "0000")
-                    {
-                        wasConverted = true;
-                        cellString = Rules[cellString];
+                    var result = string.Empty;
+                    for (var i = cellCount - 1; i >= 0; i--)
+                        result += cells[i][0];
 
-                        pyramid[pSecondRow][pSecondPosition + 2] = cellString[0];
-                        pyramid[pSecondRow][pSecondPosition + 1] = cellString[1];
-                        pyramid[pSecondRow][pSecondPosition + 0] = cellString[2];
-                        pyramid[pRow][pPosition] = cellString[3];
-                    }
+                    pyramid = GetPyramid(result);
+                    length = result.Length;
 
-                    isUpDown = !isUpDown;
-
-                    cells[cellPosition] = cellString;
-                    cellPosition++;
+                    if (length == 1)
+                        return pyramid;
                 }
             }
 
-            if (wasConverted) return pyramid;
+            if (!reduced)
+                for (var i = 0; i < cells.Length; i++)
+                {
+                    var newString = Rules[cells[i]];
+                    pyramid[coords[i, 2]][coords[i, 3] + 2] = newString[0];
+                    pyramid[coords[i, 2]][coords[i, 3] + 1] = newString[1];
+                    pyramid[coords[i, 2]][coords[i, 3] + 0] = newString[2];
+                    pyramid[coords[i, 0]][coords[i, 1]] = newString[3];
+                }
 
-            var result = string.Empty;
-
-            for (var i = cellCount - 1; i >= 0; i--)
-                result += cells[i][0];
-
-            return GetPyramid(result);
+            return pyramid;
         }
 
         private static string PyramidToString(char[][] pyramid)
@@ -140,10 +161,17 @@ namespace Tribit
             if (input.Replace("1", "").Replace("0", "").Length != 0)
                 throw new ArgumentException("Input string must cointains only '0' or '1' symbols!");
 
-            do
+            var pyramid = GetPyramid(input);
+            while (true)
             {
-                input = PyramidToString(GetPyramid(input));
-            } while (input.Length != 1);
+                input = PyramidToString(pyramid);
+                Console.WriteLine(input);
+
+                if (input.Length == 1)
+                    break;
+
+                pyramid = Convert(pyramid, input.Length);
+            }
 
             Console.WriteLine();
         }
@@ -154,8 +182,16 @@ namespace Tribit
             Process("0001001111100110");
             Process("1100100100001111110110101010001000100001011010001100001000110100");
 
+            Process("0000000000000000000000000000000000000000000000000000000000000000");
+            Process("0001111111101111111111111111111101111111111111111111011111111111");
+
+            Process("1110000000000001000000000000000000000000000000000000000000000000");
+            Process("0000111000010000");
+
             if (args.Length != 0)
                 Process(args[0]);
+
+            Console.ReadKey();
         }
     }
 }
